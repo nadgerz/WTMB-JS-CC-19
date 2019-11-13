6292aa2039f0fabc44d2c64d5d92dab06b411867
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-13 18:27:59 +0100

    5th lesson finished

---
+++ week5/lecture/models/meetup.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const MeetupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 2,
+    },
+    location: {
+        type: String,
+        required: true,
+        minLength: 2,
+    },
+    attendees: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Person',
+        autopopulate: {
+            maxDepth: 1
+        },
+    }]
+
+});
+
+//
+// MeetupSchema.methods.attend = async function(meetup) {
+//     this.meetups.push(meetup);
+//     await this.save();
+// };
+
+MeetupSchema.plugin(require('mongoose-autopopulate'));
+
+
+const MeetupModel = mongoose.model('Meetup', MeetupSchema);
+
+module.exports = MeetupModel;
---
+++ week5/lecture/routes/person.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const PersonService = require('../services/person-service');
+const MeetupService = require('../services/meetup-service');
+
+router.get('/all', async (req, res) => {
+  const people = await PersonService.findAll();
+  res.render('list', { items: people });
+});
+
+router.get('/:id', async (req, res) => {
+  const user = await PersonService.find(req.params.id);
+  res.render('data', { data: user });
+});
+
+router.post('/', async (req, res) => {
+  const user = await PersonService.add(req.body);
+  res.send(user);
+});
+
+router.delete('/:id', async (req, res) => {
+  const user = await PersonService.del(req.params.id);
+  res.send(user);
+});
+
+router.post('/:id/meetups', async (req, res) => {
+  const person = await PersonService.find(req.params.id);
+  const meetup = await MeetupService.find(req.body.meetup);
+
+  await PersonService.attendMeetup(person, meetup);
+});
+
+router.get('/:id/peers-over-18', async (req, res) => {
+  const person = await PersonService.find(req.params.id);
+  const meetup = await MeetupService.find(req.body.meetup);
+
+  await PersonService.attendMeetup(person, meetup);
+});
+module.exports = router;
