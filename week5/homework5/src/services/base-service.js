50e3d5871e0a01252d0bedc076bdb221b96d9114
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-14 18:14:05 +0100

    changed to work with mongoose calls

--- week5/homework5/src/services/base-service.js
+++ week5/homework5/src/services/base-service.js
@@ -1,72 +1,29 @@
-const fs = require('fs');
-const Flatted = require('flatted/cjs');
-
 module.exports = class Service {
-  constructor(model, dbPath) {
+  constructor(model) {
     this.model = model;
-    this.dbPath = dbPath;
   }
 
   async findAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(this.dbPath, 'utf8', async (err, file) => {
-        if (err) {
-          if (err.code === 'ENOENT') {
-            await this.saveAll([]);
-            return resolve([]);
-          }
-          return reject(err);
-        }
-        const items = Flatted.parse(file).map(this.model.create);
-
-        resolve(items);
-      });
-    });
+    return this.model.find();
   }
 
   async add(item) {
-    const allItems = await this.findAll();
-
-    allItems.push(item);
-
-    await this.saveAll(allItems);
-
-    return item;
+    return this.model.create(item);
   }
 
-  async update(updatedItem) {
-    const allItems = await this.findAll();
-    const itemIndex = allItems.findIndex(item => item.id === updatedItem.id);
-
-    if (itemIndex < 0) {
-      await this.add(updatedItem);
-    } else {
-      allItems.splice(itemIndex, 1, updatedItem);
-      await this.saveAll(allItems);
-    }
+  async del(itemId) {
+    return this.model.remove({ _id: itemId });
   }
 
-  async delete(itemId) {
-    let allItems = await this.findAll();
-
-    allItems = allItems.filter(item => item.id !== itemId);
-
-    await this.saveAll(allItems);
+  async findById(itemId) {
+    return this.model.findById(itemId);
   }
 
-  async find(itemId) {
-    const allItems = await this.findAll();
-
-    return allItems.find(item => item.id === itemId);
+  async find(query) {
+    return this.model.find(query);
   }
 
-  async saveAll(items) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(this.dbPath, Flatted.stringify(items, null, 2), err => {
-        if (err) return reject(err);
-
-        resolve();
-      });
-    });
-  }
+  // async saveAll() {
+  //   return this.model.findA();
+  // }
 };
