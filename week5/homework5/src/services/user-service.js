34fa0dbc1e812bfbc195d2260684844a8efd697d
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-14 18:15:46 +0100

    changed to work with mongoose calls

--- week5/homework5/src/services/user-service.js
+++ week5/homework5/src/services/user-service.js
@@ -1,20 +1,38 @@
 // import validator from 'validator';
-const isEmail = require('validator/lib/isEmail');
+// const isEmail = require('validator/lib/isEmail');
 const BaseService = require('./base-service');
 const UserModel = require('../models/user');
 
 class UserService extends BaseService {
   constructor() {
-    super(UserModel, `${__dirname}/../user-database.json`);
+    super(UserModel);
   }
 
-  async add({name, email, password}) {
-    if (!isEmail(email)) {
-      return null;
-    }
-    const user = new UserModel(name, email, password);
-    return await super.add(user);
+  async add() {
+    console.log('userService: add');
   }
+
+  // async add({name, email, password}) {
+  //   if (!isEmail(email)) {
+  //     return null;
+  //   }
+  //   const user = new UserModel(name, email, password);
+  //   return await super.add(user);
+  // }
 }
 
 module.exports = new UserService();
+
+//class PersonService extends BaseService {
+//   // model = PersonModel;
+//   constructor() {
+//     super(PersonModel);
+//   }
+//
+//   async attendMeetup(person, meetup) {
+//     person.meetups.push(meetup);
+//     meetup.attendees.push(person);
+//     await person.save();
+//     await meetup.save();
+//   }
+// }
