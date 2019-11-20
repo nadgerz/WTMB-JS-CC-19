329d9c97ef56ca07a9081ef2c60bb574b97cec0f
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-20 17:28:35 +0100

    improved user validation

---
+++ week6/homework6/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+// const Recipe = require('./recipe');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user name is required'],
+    // defining an error message
+    minLength: [2, 'Name must be longer than 1 letter'],
+    maxLength: [30, 'A name can only have a maximum of 30 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'An Email Address is required'],
+    validate: {
+      validator: function(valueToValidate) {
+        return isEmail(valueToValidate);
+      },
+      message: props => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'A password is required'],
+    minLength: [6, 'the minimum length is 6 characters'],
+    maxLength: [60, 'the maximum characters allowed is 30']
+  },
+  recipes: [{
+    type: mongoose.SchemaTypes.ObjectId,
+  }],
+});
+
+// UserSchema.plugin(require('mongoose-autopopulate'));
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
