daeca30c4cb96b6602bc3c0f8b2793dc8bec4ee4
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-20 15:23:40 +0100

    split up old index.js to app and index.js

---
+++ week6/homework6/index.js
@@ -0,0 +1,16 @@
+const Chalk = require('chalk');
+// const path = require('path');
+const app = require('./app');
+
+
+// Start up the server on a customer port, if required.
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.info(
+    `server has started on port ${
+      PORT === DEFAULT_PORT ? PORT : Chalk.bgYellowBright(PORT)
+    }`,
+  );
+});
