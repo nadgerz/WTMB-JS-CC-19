f43ccc97521d8dce880e744bace65339b908c4ac
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-25 12:17:56 +0100

    test/models/user test for valid user also checking that the potential error is undefined

--- week6/homework6/test/models/user.js
+++ week6/homework6/test/models/user.js
@@ -1,6 +1,3 @@
-/* eslint-disable */
-
-// const assert = require('assert');
 import test from 'ava';
 import UserModel from '../../models/user';
 
@@ -19,7 +16,8 @@ test.beforeEach(t => {
       },
       email: {
         required: 'Email is required',
-        noUnique: 'A User with this Email already exists',
+        notUnique: 'A User with this Email already exists',
+        // failedValidation: TODO: check for this as well
       },
       password: {
         required: 'Password is required',
@@ -30,11 +28,17 @@ test.beforeEach(t => {
   };
 });
 
+const getErrorMsg = (badUser, badProperty) => {
+  const error = badUser.validateSync();
+  return error.errors[badProperty].message;
+};
+
 test('creating new user with valid input', async t => {
-  t.plan(5);
-
+  t.plan(6);
   const validUser = new UserModel(t.context.user);
+  const error = validUser.validateSync();
 
+  t.falsy(error);
   t.true(Array.isArray(validUser.recipes));
   t.true(validUser.recipes.length === 0);
   t.is(validUser.name, t.context.user.name);
@@ -42,12 +46,7 @@ test('creating new user with valid input', async t => {
   t.is(validUser.password, t.context.user.password);
 });
 
-const getErrorMsg = (badUser, badProperty) => {
-  const error = badUser.validateSync();
-  return error.errors[badProperty].message;
-};
-
-test('creating a user with an invalid username', async t => {
+test('creating a user with invalid username', async t => {
   t.plan(2);
   t.context.user.name = 's';
   const badProperty = 'name';
@@ -64,71 +63,21 @@ test('creating a user with an invalid username', async t => {
   errorMsg = getErrorMsg(badUser, badProperty);
   t.is(errorMsg, errorMsgExpected);
 });
-
-// test.after.always(async () => {
-//   mongoose.disconnect();
-//   // mongod.stop();
+//
+// test('creating a user with invalid email', async t => {
+//   t.plan(2);
+//   t.context.user.email = '';
+//   const badProperty = 'email';
+//   let errorMsgExpected = t.context.errorMsgs.email.required;
+//   let badUser = new UserModel(t.context.user);
+//
+//   let errorMsg = getErrorMsg(badUser, badProperty);
+//   t.is(errorMsg, errorMsgExpected);
+//
+//   t.context.user.email = 's';
+//   errorMsgExpected = t.context.errorMsgs.email.tooLong;
+//   badUser = new UserModel(t.context.user);
+//
+//   errorMsg = getErrorMsg(badUser, badProperty);
+//   t.is(errorMsg, errorMsgExpected);
 // });
-
-// test('authenticating with an invalid password', async t => {
-//   t.context.credentials.password = 'bad_password';
-//   const isValid = t.context.authenticator.authenticate(t.context.credentials);
-//   t.false(await isValid);
-// });
-
-// test('Create new user with UserModel', t => {
-//   t.plan(5);
-//
-//   const validUser = {
-//     name: 'steve',
-//     email: 'steve@mail.com',
-//     password: '123567',
-//   };
-//
-//   const createdUser = new UserModel(validUser);
-//
-//
-//   t.true(Array.isArray(createdUser.recipes));
-//   t.true(createdUser.recipes.length === 0);
-//   t.is(createdUser.name, validUser.name);
-//   t.is(createdUser.email, validUser.email);
-//   t.is(createdUser.password, validUser.password);
-// });
-//
-// test('fail at creating a new user with UserModel', t => {
-//   t.plan(3);
-//
-//   const badUser = {
-//     name: 's',
-//     email: 's@',
-//     password: '12356',
-//   };
-//
-//   // const createdUser = new UserModel(badUser);
-//
-//   // validateSync();
-//   // Returns:
-//   // «ValidationError,undefined» ValidationError if there are errors during validation, or undefined if there is no error.
-//   // This method is useful if you need synchronous validation.
-//
-//   // let error = validUser.validateSync();
-//
-//   // console.log(error.errors['email']);
-//   // assert.equal(error.errors['email'].message,
-//   //   `${validUser.email} is not a valid email address!`);
-//
-//   console.log('recipes');
-//   console.log(createdUser.recipes);
-//
-//   t.is(createdUser.name, validUser.name);
-//   t.is(createdUser.email, validUser.email);
-//   t.is(createdUser.password, validUser.password);
-// });
-
-// drink: {
-//   type: String,
-// enum: ['Coffee', 'Tea'],
-//     required: function() {
-//     return this.bacon > 3;
-//   }
-// }
