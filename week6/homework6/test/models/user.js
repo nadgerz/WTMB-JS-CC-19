ddb2c3ba120ad2a2c282f76b4f3d66618ecc6504
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-25 12:22:49 +0100

    test/models/user test for invalid username now checks for required field err Msg

--- week6/homework6/test/models/user.js
+++ week6/homework6/test/models/user.js
@@ -35,31 +35,38 @@ const getErrorMsg = (badUser, badProperty) => {
 
 test('creating new user with valid input', async t => {
   t.plan(6);
+
   const validUser = new UserModel(t.context.user);
   const error = validUser.validateSync();
 
   t.falsy(error);
   t.true(Array.isArray(validUser.recipes));
   t.true(validUser.recipes.length === 0);
+  // TODO SAI: for how many things should I check here?
   t.is(validUser.name, t.context.user.name);
   t.is(validUser.email, t.context.user.email);
   t.is(validUser.password, t.context.user.password);
 });
 
 test('creating a user with invalid username', async t => {
-  t.plan(2);
-  t.context.user.name = 's';
+  t.plan(3);
   const badProperty = 'name';
-  let errorMsgExpected = t.context.errorMsgs.name.tooShort;
-  let badUser = new UserModel(t.context.user);
 
+  t.context.user.name = null;
+  let errorMsgExpected = t.context.errorMsgs.name.required;
+  let badUser = new UserModel(t.context.user);
   let errorMsg = getErrorMsg(badUser, badProperty);
+  t.is(errorMsg, errorMsgExpected);
+
+  t.context.user.name = 's';
+  errorMsgExpected = t.context.errorMsgs.name.tooShort;
+  badUser = new UserModel(t.context.user);
+  errorMsg = getErrorMsg(badUser, badProperty);
   t.is(errorMsg, errorMsgExpected);
 
   t.context.user.name = 'sssssssssssssssssssssssssssssss';
   errorMsgExpected = t.context.errorMsgs.name.tooLong;
   badUser = new UserModel(t.context.user);
-
   errorMsg = getErrorMsg(badUser, badProperty);
   t.is(errorMsg, errorMsgExpected);
 });
